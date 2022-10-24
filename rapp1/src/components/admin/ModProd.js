0ee5b98cfc5c2e2ba398a3ba544fc396db674ddb
DanielCorrea30 <dcn.91@hotmail.com>
2022-10-23 19:45:35 -0500

    actualización de Modificar

--- rapp1/src/components/admin/ModProd.js
+++ rapp1/src/components/admin/ModProd.js
@@ -1,15 +1,52 @@
+import { productsData } from "../../Data/ProductsData";
 import "../../styles/style.css"
+import { useState } from "react";
+
 export const ModProd = () =>{
+    const [id, setId] = useState(0);
+    const HandleClick = (id) => {
+        setId(id);
+    }
     return (
-        <div className="card" >
-            <div className="card-header">
-                Productos
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col">
+                    <div className="card" >
+                        <div className="card-header">
+                            Productos
+                        </div>
+                        <ul className="list-group list-group-flush" >
+                            {productsData.map((data, key) => {
+                                return (
+                                    <div key={key}>
+                                    <Producto
+                                        key={key}
+                                        nombre={data.nombre}   
+                                    />
+                                    </div>
+                                    );
+                            })}
+                        </ul>
+                    </div>,
+                </div>
+                <div className="col">
+                    <div className="card" >
+                        <img src={productsData[id].img} className="card-img-top" alt="no carga"/>
+                        <div className="card-body">
+                            <h5 className="card-title">{productsData[id].nombre}</h5>
+                        </div>
+                    </div>
+                </div>
+                       
             </div>
-            <ul className="list-group list-group-flush">
-                <li className="list-group-item">Sol de espuma</li>
-                <li className="list-group-item">Corazón juguete</li>
-                <li className="list-group-item">Otro producto</li>
-            </ul>
-            </div>
+        </div>
+    )
+}
+
+
+
+const Producto = ({nombre}) => {
+    return (
+        <li className="list-group-item">{nombre}</li>
     )
 }
