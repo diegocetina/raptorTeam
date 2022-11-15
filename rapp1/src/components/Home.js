95ec85a90944eda3063afd6a4906eb19670d1980
DIEGO CETINA <diego.cetina@hotmail.es>
2022-11-14 21:18:04 -0500

    primero cambios de estilos en la home

--- rapp1/src/components/Home.js
+++ rapp1/src/components/Home.js
@@ -1,21 +1,45 @@
 import raptor from "../raptor.png";
 import { Link } from "react-router-dom";
+import './homeStyle.css';
 export const Home = () => {
   return (
     <div>
-      <img src={raptor} alt="logo"></img>
-      <h1>Bienvenido a la app de raptorTeam</h1>
-      <h2>Escoja su rol</h2>
-      <nav>
+    <nav className="navigationBar">
+      <bars>
+        <navMenu>
         <Link to="/admin" className="navbar-brand">
           Administrador
         </Link>
-      </nav>
-      <nav>
         <Link to="/client" className="navbar-brand">
           Cliente
         </Link>
-      </nav>
+        </navMenu>
+      </bars>
+    </nav>
+    <div>
+      <h1 className="titulo">Bienvenido a la app de raptorTeam</h1>
+      <h2>Escoja su rol</h2>
     </div>
+    </div>
+
+
+    
   );
 };
+
+
+/* <div>
+<img src={raptor} alt="logo"></img>
+<h1>Bienvenido a la app de raptorTeam</h1>
+<h2>Escoja su rol</h2>
+  <nav>
+    <Link to="/admin" className="navbar-brand">
+      Administrador
+    </Link>
+  </nav>
+  <nav>
+    <Link to="/client" className="navbar-brand">
+      Cliente
+    </Link>
+  </nav>
+</div> */
