55761bf126b1b98e8b0990403da4e9d90f53a294
Diego Cetina <diego.cetina@hotmail.es>
2022-11-17 10:28:22 -0500

    nueva pagina home con estilos nuevos y nuevo logo

--- rapp1/src/components/Home.js
+++ rapp1/src/components/Home.js
@@ -1,29 +1,25 @@
-import raptor from "../raptor.png";
+import avion from "../avion.png";
 import { Link } from "react-router-dom";
 import './homeStyle.css';
+
+
 export const Home = () => {
   return (
     <div>
-    <nav className="navigationBar">
-      <bars>
-        <navMenu>
-        <Link to="/admin" className="navbar-brand">
-          Administrador
-        </Link>
-        <Link to="/client" className="navbar-brand">
-          Cliente
-        </Link>
-        </navMenu>
-      </bars>
-    </nav>
-    <div>
-      <h1 className="titulo">Bienvenido a la app de raptorTeam</h1>
-      <h2>Escoja su rol</h2>
+      <nav className="nav">
+        <a href="./home" className="site-title">RAPTOR TEAM</a>
+        <ul>
+          <li>
+            <a href="./admin">ADMINISTRADOR</a>
+          </li>
+          <li>
+            <a href="./client">CLIENTE</a>
+          </li>
+        </ul>
+      </nav>
+      <img className="logo" src={avion} alt="logo"></img>
+      <h1>BIENDENIDO A LA APP DE RAPTOR TEAM</h1>
     </div>
-    </div>
-
-
-    
   );
 };
 
